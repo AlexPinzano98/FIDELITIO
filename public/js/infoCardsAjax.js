ccc1d2b3c84b61b54896303d9ba849dd25bc2869
JordiMesas <67053157+JordiMesas@users.noreply.github.com>
2021-04-09 18:12:44 +0200

    v4.0.8

--- public/js/infoCardsAjax.js
+++ public/js/infoCardsAjax.js
@@ -21,46 +21,46 @@ function objetoAjax() {
 
 function showCard() {
     var containCards = document.getElementsByClassName('swiper-wrapper')[0];
-    var section = document.getElementById('cards');
+    // var section = document.getElementById('cards');
     var ajax = new objetoAjax();
-    var token = document.getElementById('token').getAttribute('content');
+    // var token = document.getElementById('token').getAttribute('content');
     ajax.open("GET", "showCard", true);
-    var datasend = new FormData();
-    datasend.append('_token', token);
+    // var datasend = new FormData();
+    // datasend.append('_token', token);
     ajax.onreadystatechange = function() {
         if (ajax.readyState == 4 && ajax.status == 200) {
             var response = JSON.parse(ajax.responseText);
             console.log(response);
             for (let i = 0; i < response.length; i++) {
-                section.innerHTML += `
-                <div class="swiper-slide">
-                <div class="card">
-                    <div class="card-body">
-                        <img src="img/cafe.png" class="card-img-top" alt="perfil">
-                    </div>
-                    <div class="card-stamp">
-                        <h5 class="card-title">${response[i].name}</h5>
-                        <p class="card-text">${response[i].reward}</p>
-                        <h5 class="stamp-title">Sellos de la promoción</h5>
-                        <div class="card-stamp_grid">
-                            <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
-                            <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
-                            <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
-                            <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
-                            <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
-                            <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
-                            <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
-                            <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
-                            <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
-                            <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
+                containCards.innerHTML += `
+              <div class="swiper-slide">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src="img/cafe.png" class="card-img-top" alt="perfil">
+                        </div>
+                        <div class="card-stamp">
+                            <h5 class="card-title">${response[i].name}</h5>
+                            <p class="card-text">${response[i].reward}</p>
+                            <h5 class="stamp-title">Sellos de la promoción</h5>
+                            <div class="card-stamp_grid">
+                                <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
+                                <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
+                                <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
+                                <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
+                                <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
+                                <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
+                                <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
+                                <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
+                                <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
+                                <img src="img/coffee.svg" class="img-thumbnail" alt="sello">
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>`;
+                </div>`;
             }
         }
     }
-    ajax.send(datasend);
+    ajax.send();
 }
 
 
--- resources/views/viewCliente.blade.php
+++ resources/views/viewCliente.blade.php
@@ -8,15 +8,14 @@
     <!-- Link Swiper's CSS -->
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
     <link rel="stylesheet" href="css/cardStyle.css" />
-    <script src="js/infoCardsAjax.js"></script>
     <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
 </head>
 
 <body>
     <!-- Swiper -->
     <div class="swiper-container">
-        <div class="swiper-wrapper" id="cards">
-            
+        <div class="swiper-wrapper">
+
         </div>
         <!-- Add Pagination -->
         <div class="swiper-pagination"></div>
@@ -44,7 +43,7 @@
         },
     });
     </script>
-    
+    <script src="js/infoCardsAjax.js"></script>
 </body>
 
 </html>
