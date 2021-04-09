716d649102deff56a71d8e2ef6faf92933030b63
Pablo Soriano Antón <10006712.joan23@fje.edu>
2021-04-09 15:14:13 +0200

    v4.0.1

--- resources/views/viewCliente.blade.php
+++ resources/views/viewCliente.blade.php
@@ -5,32 +5,9 @@
     <meta charset="utf-8">
     <title>cliente</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-
     <!-- Link Swiper's CSS -->
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
     <link rel="stylesheet" href="css/cardStyle.css" />
-
-    <!-- Demo styles -->
-    <style>
-    body {
-        background: #fff;
-        font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
-        font-size: 14px;
-        color: #000;
-        margin: 0;
-        padding: 0;
-    }
-
-    .swiper-container {
-        width: 100%;
-        padding-top: 50px;
-        padding-bottom: 50px;
-    }
-
-    .swiper-slide {
-        width: 300px;
-    }
-    </style>
 </head>
 
 <body>
