2f69ae6db60f61ccfd01386db35b57ed35275eb2
Pablo Soriano Antón <10006712.joan23@fje.edu>
2021-04-12 16:09:39 +0200

    v5.0.2

--- resources/views/viewCliente.blade.php
+++ resources/views/viewCliente.blade.php
@@ -8,15 +8,43 @@
     <!-- Link Swiper's CSS -->
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css" />
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="css/cardStyle.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{asset('css/cardStyle.css')}}">
+    <link rel="stylesheet" href="{{asset('css/cliente.css')}}">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="js/infoCardsAjax.js"></script>
     <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
+    <script type="text/javascript">
+		// A $( document ).ready() block.
+		$( document ).ready(function() {
+		    $('#menu_on').click(function(){
+		    	$('body').toggleClass('visible_menu');
+		    })
+		});
+	</script>
 </head>
 
 <body>
-
+		<header id="#header">
+			<a href="#" id="menu_on">
+				<span></span>
+				<span></span>
+				<span></span>
+			</a>
+		</header>
+		<nav class="navbar">	
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Servicios</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Contacto</a></li>
+			</ul>
+		</nav>
     <!-- Swiper -->
-    <div class="swiper-container">
+    <div class="swiper-container" id="content">
         <div class="swiper-wrapper">
 
         </div>
