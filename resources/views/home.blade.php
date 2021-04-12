eaba25b3c5b46b268b9f2a76bf97f15a528e3832
Danielruedamorillas <danirgames8@gmail.com>
2021-04-12 18:10:45 +0200

    v5.0.4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <button id="camara" onclick="openCamara()">CAMARA</button>
     <video id="preview" style="width: 100%; height: 100vh; display: none;"></video>
     <script>
-        function alex(){
+        function openCamara(){
             document.getElementById('preview').style.display = "block"
         }
         let scanner = new Instascan.Scanner(
@@ -17,7 +17,8 @@
             }
         );
         scanner.addListener('scan', function(content) {
-            alert('Contenido: ' + content);
+            //alert('Contenido: ' + content);
+            nepe(content);
         });
         Instascan.Camera.getCameras().then(cameras => 
         {
@@ -27,6 +28,9 @@
                 console.error("No existe cámara en el dispositivo!");
             }
         });
+        function nepe(content){
+            alert('chupamela');
+        }
     </script>
 
  </body>
