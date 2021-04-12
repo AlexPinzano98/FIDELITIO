d7665a3fd5b6b33ccdde5578a0c5be3f54ead4f6
Alex Pinzano de la Torre <alexpinzanodelatorre@MacBook-Pro-de-Alex.local>
2021-04-12 17:37:12 +0200

    v1.1.1.1.2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,12 @@
     <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js" ></script>	
   </head>
   <body>
-    <video id="preview" style="width: 100%; height: 100vh;"></video>
+    <button id="camara" onclick="openCamara()">CAMARA</button>
+    <video id="preview" style="width: 100%; height: 100vh; display: none;"></video>
     <script>
+        function alex(){
+            document.getElementById('preview').style.display = "block"
+        }
         let scanner = new Instascan.Scanner(
             {
                 video: document.getElementById('preview')
@@ -14,14 +18,13 @@
         );
         scanner.addListener('scan', function(content) {
             alert('Contenido: ' + content);
-            //window.open(content, "_blank");
         });
         Instascan.Camera.getCameras().then(cameras => 
         {
             if(cameras.length > 0){
                 scanner.start(cameras[0]);
             } else {
-                console.error("Não existe câmera no dispositivo!");
+                console.error("No existe cámara en el dispositivo!");
             }
         });
     </script>
