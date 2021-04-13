1944cd615e9a2948e5e292b6a16b130c3b1c755d
Pablo Soriano Antón <10006712.joan23@fje.edu>
2021-04-13 17:05:47 +0200

    v5.1.4

--- resources/views/viewCliente.blade.php
+++ resources/views/viewCliente.blade.php
@@ -3,20 +3,18 @@
 
 <head>
     <meta charset="utf-8">
-    <title>cliente</title>
+    <title>FIDELITIO</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
     <!-- Link Swiper's CSS -->
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css" />
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
     <link rel="stylesheet" href="{{asset('css/cardStyle.css')}}">
     <link rel="stylesheet" href="{{asset('css/cliente.css')}}">
-    <!-- <link rel="stylesheet" href="{{asset('css/cliente.css')}}"> -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="js/infoCardsAjax.js"></script>
     <script src="https://kit.fontawesome.com/55e6be5a81.js" crossorigin="anonymous"></script>
     <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
     <script type="text/javascript">
-		// A $( document ).ready() block.
 		$( document ).ready(function() {
 		    $('#menu_on').click(function(){
 		    	$('body').toggleClass('visible_menu');
@@ -55,12 +53,11 @@
         <div class="swiper-pagination"></div>
     </div>
 </div>
-     <!-- Swiper JS -->
-     <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+
+    <!-- Swiper JS -->
+    <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <!-- Initialize Swiper -->
-
-    
 
 </body>
 </html>
