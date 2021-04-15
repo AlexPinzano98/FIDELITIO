fae4d54b539d1f6cbb8b17e1435961b79a75ae8c
Danielruedamorillas <danirgames8@gmail.com>
2021-04-15 15:57:58 +0200

    v5.3.3

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -23,7 +23,9 @@
             <div class="mb-3">
                 <input name="psswd" type="password" class="form-control" id="contrasenya" placeholder="Contrasenya..."></input>
             </div>
-
+            <button type="submit" id="submit" class="btn btn-warning" style="margin-top:15%;">
+                Inicia Sesión
+            </button>
             <hr>
 
             <button type="button" class="btn btn-primary">
@@ -32,9 +34,7 @@
             <button type="button" class="btn btn-info">
                 Continuar con Google
             </button>
-            <button type="submit" id="submit" class="btn btn-warning" style="margin-top:15%;">
-                Inicia Sesión
-            </button>
+
 
             <div id="message">
             </div>
