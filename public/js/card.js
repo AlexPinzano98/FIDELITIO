39fef276587274a09bbed4b07829fa4c492e4654
MartinezFalconi <sergiomafa09@gmail.com>
2021-04-20 17:19:13 +0200

    v6.2.4

--- public/js/card.js
+++ public/js/card.js
@@ -60,7 +60,6 @@ function sellar(content) {
     var minute = array[8];
 
     var now = new Date();
-<<<<<<< HEAD
     var year_now = now.getFullYear();
     var month_now = now.getMonth() + 1;
     var day_now = now.getDate();
@@ -85,99 +84,85 @@ function sellar(content) {
             alert('QR CADUCADO');
         } else if (year <= year_now && month <= month_now && day <= day_now && hour == hour_now && minute < minute_now) {
             alert('QR valido');
-=======
-    var year_now=now.getFullYear();
-    var month_now=now.getMonth()+1;
-    var day_now=now.getDate();
-    var hour_now=now.getHours();
-    var minute_now=now.getMinutes()-2;
-    var fecha_qr=new Date(year,month,day,hour,minute)
-    var fecha_actual=new Date(year_now,month_now,day_now,hour_now,minute_now)
-    // console.log(fecha_actual)
-    // console.log(fecha_qr)
-    if(year!=""){
-        if(fecha_actual.getTime()<fecha_qr.getTime()){
-            alert('qr valido')
->>>>>>> ac1ce6e66f8e4d481924a79b8f4a7b42b25b0458
-            closeCamara();
-            read();
-<<<<<<< HEAD
-        } else {
-            alert('Este QR no es valido')
-                // Msg error
-        }
-    }
-=======
-        }else{
-            alert('qr expirado')
-        }
-        }else{
-            alert('Este QR no es valido')
-            // Msg error
+            var year_now = now.getFullYear();
+            var month_now = now.getMonth() + 1;
+            var day_now = now.getDate();
+            var hour_now = now.getHours();
+            var minute_now = now.getMinutes() - 2;
+            var fecha_qr = new Date(year, month, day, hour, minute)
+            var fecha_actual = new Date(year_now, month_now, day_now, hour_now, minute_now)
+                // console.log(fecha_actual)
+                // console.log(fecha_qr)
+            if (year != "") {
+                if (fecha_actual.getTime() < fecha_qr.getTime()) {
+                    alert('qr valido')
+                    closeCamara();
+                    read();
+                } else {
+                    alert('qr expirado')
+                }
+            } else {
+                alert('Este QR no es valido')
+                    // Msg error
             }
-    // if(year!=""){
-    //     if (year < year_now) {
-    //     alert('QR CADUCADO');
-    //     } else if (year <= year_now && month < month_now) {
-    //         alert('QR CADUCADO');
-    //     } else if (year <= year_now && month <= month_now && day < day_now) {
-    //         alert('QR CADUCADO');
-    //     } else if (year <= year_now && month <= month_now && day <= day_now && hour < hour_now) {
-    //         alert('QR CADUCADO');
-    //     } else if (year <= year_now && month <= month_now && day <= day_now && hour == hour_now && minute <= minute_now) {
-    //         alert('QR CADUCADO');
-    //     } else if (year <= year_now && month <= month_now && day <= day_now && hour == hour_now && minute > minute_now) {
-    //         alert('QR valido');
-    //         closeCamara();
-    //         closeModal();
-    //         // Ajax
-    //         read();
-    //     }else{
-    //         alert('Este QR no es valido')
-    //         // Msg error
-    //     }
-    //}
->>>>>>> ac1ce6e66f8e4d481924a79b8f4a7b42b25b0458
+            // if(year!=""){
+            //     if (year < year_now) {
+            //     alert('QR CADUCADO');
+            //     } else if (year <= year_now && month < month_now) {
+            //         alert('QR CADUCADO');
+            //     } else if (year <= year_now && month <= month_now && day < day_now) {
+            //         alert('QR CADUCADO');
+            //     } else if (year <= year_now && month <= month_now && day <= day_now && hour < hour_now) {
+            //         alert('QR CADUCADO');
+            //     } else if (year <= year_now && month <= month_now && day <= day_now && hour == hour_now && minute <= minute_now) {
+            //         alert('QR CADUCADO');
+            //     } else if (year <= year_now && month <= month_now && day <= day_now && hour == hour_now && minute > minute_now) {
+            //         alert('QR valido');
+            //         closeCamara();
+            //         closeModal();
+            //         // Ajax
+            //         read();
+            //     }else{
+            //         alert('Este QR no es valido')
+            //         // Msg error
+            //     }
+            //}
 
-    function read() {
-        // var section = document.getElementById('section-3');
-        var ajax = new objetoAjax();
-        var token = document.getElementById('token').getAttribute('content');
-        // Busca la ruta read y que sea asyncrono
-        ajax.open('POST', 'validarQR', true);
-        var datasend = new FormData();
-        datasend.append('_token', token);
-        datasend.append('id_promo', id_promo);
-        datasend.append('id_camarero', id_camarero);
+            function read() {
+                // var section = document.getElementById('section-3');
+                var ajax = new objetoAjax();
+                var token = document.getElementById('token').getAttribute('content');
+                // Busca la ruta read y que sea asyncrono
+                ajax.open('POST', 'validarQR', true);
+                var datasend = new FormData();
+                datasend.append('_token', token);
+                datasend.append('id_promo', id_promo);
+                datasend.append('id_camarero', id_camarero);
 
-        ajax.onreadystatechange = function() {
-            if (ajax.readyState == 4 && ajax.status == 200) {
-                var respuesta = JSON.parse(ajax.responseText);
-                // var tabla = '';
-                console.log(respuesta)
-<<<<<<< HEAD
-                showCard();
-                // section.innerHTML = tabla;
-=======
-               // section.innerHTML = tabla;
-               showCard();
->>>>>>> ac1ce6e66f8e4d481924a79b8f4a7b42b25b0458
+                ajax.onreadystatechange = function() {
+                    if (ajax.readyState == 4 && ajax.status == 200) {
+                        var respuesta = JSON.parse(ajax.responseText);
+                        // var tabla = '';
+                        console.log(respuesta)
+                        showCard();
+                        // section.innerHTML = tabla;
+                        // section.innerHTML = tabla;
+                        showCard();
+                    }
+                }
+                ajax.send(datasend);
             }
-        }
-        ajax.send(datasend);
-    }
 
-    // Hacer llamada AJAX al método de validación QR
+            // Hacer llamada AJAX al método de validación QR
 
-    // if (fecha_actual.getTime() < fecha_qr.getTime()) {
-    //     alert('QR CADUCADO');
-    //  }
-    //alert(id_local);
-    // var token = document.getElementById("token").getAttribute("content");
-    // var ajax = new objetoAjax();
-    // ajax.open('POST', 'ver_promociones', true);
-    // var datasend = new FormData();
-    // datasend.append('id_local', id_local);
-    // datasend.append('_token', token);
-    //alert('Contenido: ' + content);
-}
+            // if (fecha_actual.getTime() < fecha_qr.getTime()) {
+            //     alert('QR CADUCADO');
+            //  }
+            //alert(id_local);
+            // var token = document.getElementById("token").getAttribute("content");
+            // var ajax = new objetoAjax();
+            // ajax.open('POST', 'ver_promociones', true);
+            // var datasend = new FormData();
+            // datasend.append('id_local', id_local);
+            // datasend.append('_token', token);
+            //alert('Contenido: ' + content);
