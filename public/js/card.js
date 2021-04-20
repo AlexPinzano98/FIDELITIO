4af8389ae73ec42058b47468edfc54eb1d14ae1a
MartinezFalconi <sergiomafa09@gmail.com>
2021-04-20 16:57:48 +0200

    v6.2.2

--- public/js/card.js
+++ public/js/card.js
@@ -14,21 +14,18 @@ function objetoAjax() {
     }
     return xmlhttp;
 }
-let scanner = new Instascan.Scanner(
-    {
-        video: document.getElementById('preview')
-    }
-);
+let scanner = new Instascan.Scanner({
+    video: document.getElementById('preview')
+});
 scanner.addListener('scan', function(content) {
     alert('Contenido: ' + content);
     sellar(content);
 });
 
 
-function openCamara(){
-    Instascan.Camera.getCameras().then(cameras =>
-    {
-        if(cameras.length > 0){
+function openCamara() {
+    Instascan.Camera.getCameras().then(cameras => {
+        if (cameras.length > 0) {
             scanner.start(cameras[0]);
         } else {
             console.error("No existe cámara en el dispositivo!");
@@ -37,42 +34,45 @@ function openCamara(){
     document.getElementById('modal2').style.display = "block";
     document.getElementById('preview').style.display = "block";
 }
-function closeCamara(){
+
+function closeCamara() {
     scanner.stop();
     document.getElementById('modal2').style.display = "none";
     document.getElementById('preview').style.display = "none";
-} 
+}
+
 function closeModal() {
     document.getElementById('modal2').style.display = "none";
     closeCamara();
 }
-function sellar(content){
+
+function sellar(content) {
     const array = content.split(',');
     //alert(array[1]);
-    var id_promo=array[2];
-    var id_camarero=array[3];
+    var id_promo = array[2];
+    var id_camarero = array[3];
     //alert(array[2]);
-    var year=array[4];
-    var month=array[5];
-    var day=array[6];
-    var hour=array[7];
-    var minute=array[8];
+    var year = array[4];
+    var month = array[5];
+    var day = array[6];
+    var hour = array[7];
+    var minute = array[8];
 
     var now = new Date();
-    var year_now=now.getFullYear();
-    var month_now=now.getMonth()+1;
-    var day_now=now.getDate();
-    var hour_now=now.getHours();
-    var minute_now=now.getMinutes()+2;
+    var year_now = now.getFullYear();
+    var month_now = now.getMonth() + 1;
+    var day_now = now.getDate();
+    var hour_now = now.getHours();
+    var minute_now = now.getMinutes() + 2;
     // console.log(minute)
     // console.log(minute_now)
     // var fecha_qr=new Date(year,month,day,hour,minute)
     // var fecha_actual=new Date(year_now,month_now,day_now,hour_now,minute_now)
     // console.log(fecha_actual.getTime())
     // console.log(fecha_qr.getTime())
-    if(year!=""){
+    if (year != "") {
         if (year < year_now) {
-        alert('QR CADUCADO');
+            alert('QR CADUCADO');
         } else if (year <= year_now && month < month_now) {
             alert('QR CADUCADO');
         } else if (year <= year_now && month <= month_now && day < day_now) {
@@ -87,9 +87,9 @@ function sellar(content){
             closeModal();
             // Ajax
             read();
-        }else{
+        } else {
             alert('Este QR no es valido')
-            // Msg error
+                // Msg error
         }
     }
 
@@ -109,7 +109,8 @@ function sellar(content){
                 var respuesta = JSON.parse(ajax.responseText);
                 // var tabla = '';
                 console.log(respuesta)
-               // section.innerHTML = tabla;
+                showCard();
+                // section.innerHTML = tabla;
             }
         }
         ajax.send(datasend);
@@ -127,5 +128,5 @@ function sellar(content){
     // var datasend = new FormData();
     // datasend.append('id_local', id_local);
     // datasend.append('_token', token);
-        //alert('Contenido: ' + content);
+    //alert('Contenido: ' + content);
 }
