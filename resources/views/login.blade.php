1ddec1385a8a2a2a8a51aaa8c26ee9aa22a7febb
Pablo Soriano Antón <10006712.joan23@fje.edu>
2021-04-21 02:09:11 +0200

    v6.1.10

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,7 +21,7 @@
 
 <body>
    <!--  <p class="h1">Fidelitio</p> -->
-    <img src="img/stimpa.jpeg" style="width: 80%; margin-left: 10%; margin-top: 20px">
+    <img src="img/stimpa.png" style="width: 60%; margin-left: 20%; margin-top: 2%;">
     <div class="login">
         <form action="{{url('/validarlogin')}}" method="POST" onsubmit="return validarForm()">
             {{csrf_field()}}
