550eb27a500f119d88e2da91e0669045f17e4994
Alex Pinzano de la Torre <alexpinzanodelatorre@MacBook-Pro-de-Alex.local>
2021-05-27 03:20:45 +0200

    v26.10.4

--- resources/views/crudPromociones_Local.blade.php
+++ resources/views/crudPromociones_Local.blade.php
@@ -132,12 +132,8 @@
     <!-- FORMULARIO PARA ACTUALIZAR UNA PROMOCIÓN -->
     <div id="actualizar" class="actualizar" style="display: none;"> 
         <h1> ACTUALIZA UNA PROMOCIÓN</h1>
-<<<<<<< HEAD
         <button onclick="closeUpdate()">CANCELAR</button>
              
-=======
-        <button onclick="closeRegister()">CANCELAR</button>
->>>>>>> 131ecb69d2bb2d7890281c5fbd88f66250960c41
             <div>
                 <input type="hidden" name="id_promo" id="id_promo" readonly>
                 <p>Icono de la promoción</p>
@@ -147,7 +143,6 @@
                 <p>Premio</p>
                 <input name="premioa" type="text" id="premioa"></input>
                 <p>Estampados máximos</p>
-<<<<<<< HEAD
                 <input name="sellosa" type="number" id="sellosa" readonly></input>
                 <p>La promoción será ilimitada?</p>
                 <div>
@@ -167,19 +162,6 @@
             </div>
        
         <button type="submit" id="submit" class="btn btn-warning" onclick="actualizar_promo()">
-=======
-                <input name="sellosa" type="number" id="sellosa" placeholder="Sellos máximos..."></input>
-                <p>Fecha expiración</p>
-                <input name="fechaa" type="date" id="fechaa" placeholder="Fecha expiración..."></input>
-                <p>Restaurante</p>
-                <select class="form-control" id="restaurantea" name="restaurantea">
-                    <option selected disabled value="">Seleccione su restaurante</option>
-                </select>
-                <p>Email</p>
-                <input name="emaila" type="email" id="emaila" placeholder="Email..."></input>
-            </div>
-        <button type="submit" id="submit" class="btn btn-warning" onclick="registrar_promo()">
->>>>>>> 131ecb69d2bb2d7890281c5fbd88f66250960c41
             Actualizar promocion
         </button>
         <div id="message">
@@ -188,15 +170,18 @@
     </div>
 
     <!-- TABLA QUE CONTENDRÁ TODOS LOS DATOS DE LAS PROMOCIONES -->
-<<<<<<< HEAD
-    <div class="datos">
-        <p>Num de resultados</p>
-        <select id="results" name="results" onchange="mostrar_datos()">
-            <option selected value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
-        </select>
-        <table>
+    <div class="crud" id="content">
+        <div class="datos">
+            <button id="btn-register" onclick="openRegister()"><i class="fas fa-clone"></i>Añadir promoción</button>
+            <button id="btn-register-icon" onclick="openRegisterIcons()"><i class="fas fa-magic"></i>Registrar Icono</button>
+            <p id="total_datos"></p>
+            <p id="num">Num de resultados</p>
+            <select id="results" name="results" onchange="mostrar_datos()">
+                <option selected value="5">5</option>
+                <option value="10">10</option>
+                <option value="20">20</option>
+            </select>
+            <table>
             <thead>
                 <tr>
                     <th>Nº sellos</th>
@@ -239,63 +224,6 @@
             <tbody id="datos">
             </tbody>
         </table>
-        <div>
-=======
-    <div class="crud" id="content">
-        <div class="datos">
-            <button id="btn-register" onclick="openRegister()"><i class="fas fa-clone"></i>Añadir promoción</button>
-            <button id="btn-register-icon" onclick="openRegisterIcons()"><i class="fas fa-magic"></i>Registrar Icono</button>
->>>>>>> 131ecb69d2bb2d7890281c5fbd88f66250960c41
-            <p id="total_datos"></p>
-            <p id="num">Num de resultados</p>
-            <select id="results" name="results" onchange="mostrar_datos()">
-                <option selected value="5">5</option>
-                <option value="10">10</option>
-                <option value="20">20</option>
-            </select>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nº sellos</th>
-                        <th>Premio</th>
-                        <th>Nombre</th>
-                        <th>Expiracion</th>
-                        <th>Ilimitada</th> 
-                        <th>Local</th>
-                        <th>Usuario</th>
-                        <th>Status</th>
-                        <th>Inicio</th>
-                        <th>Final</th>
-                        <th colspan="2">Acciones</th>
-                    </tr>
-                    <tr>
-                        <th><input type="number" name="f_sellos" id="f_sellos" onkeyup="ver_promociones()"></th>
-                        <th><input type="text" name="f_premio" id="f_premio" onkeyup="ver_promociones()"></th>
-                        <th><input type="text" name="f_nombre" id="f_nombre" onkeyup="ver_promociones()"></th>
-                        <th><input type="date" name="f_fecha" id="f_fecha" onkeyup="ver_promociones()"></th>
-                        <th><select class="form-control" id="f_ilimitada" name="f_ilimitada" onclick="ver_promociones()">
-                                <option selected value="">-</option>
-                                <option value="Si">Si</option>
-                                <option value="No">No</option>
-                            </select> 
-                        </th>
-                        <th><input type="text" name="f_local" id="f_local" onkeyup="ver_promociones()"></th>
-                        <th><input type="text" name="f_email" id="f_email" onkeyup="ver_promociones()"></th>
-                        <th><select class="form-control" id="f_status" name="f_status" onclick="ver_promociones()">
-                                <option selected value="">-</option>
-                                <option value="enable">Enable</option>
-                                <option value="disable">Disable</option>
-                            </select> 
-                        </th>
-                        <th></th>
-                        <th></th>
-                        <th>Modificar</th>
-                        <th>Eliminar</th>
-                    </tr>
-                </thead>
-                <tbody id="datos">
-                </tbody>
-            </table>
             <div id="pag">
                 <button id="prev" onclick="prev()"><i class="fas fa-arrow-left"></i>Anterior</button>
                 <button id="next" onclick="next()">Siguiente<i class="fas fa-arrow-right"></i></button>
